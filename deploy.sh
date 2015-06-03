fe6a2057670a80f74279c5414be92f5a2a3105be
Sebastian Gibb <mail@sebastiangibb.de>
2015-06-03 21:14:26 +0200

    add travis + drat deploy script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# taken from https://github.com/csgillespie/dratTravis/blob/master/deploy.sh
+
+set -o errexit -o nounset
+
+addToDrat() {
+  PKG_REPO=$PWD
+  cd ..; mkdir drat; cd drat
+
+  ## Set up Repo parameters
+  git init
+  git config user.name "Sebastian Gibb"
+  git config user.email "mail@sebastiangibb.de"
+  git config --global push.default simple
+
+  ## Get drat repo
+  git remote add upstream "https://$GH_TOKEN@github.com/sgibb/drat.git"
+  git fetch upstream
+  git checkout gh-pages
+
+  Rscript -e "drat::insertPackage('$PKG_REPO/$PKG_TARBALL', \
+    repodir = '.', \
+    commit = 'Update $PKG_TARBALL (travis build $TRAVIS_BUILD_NUMBER)')"
+  git push
+}
+
+addToDrat
